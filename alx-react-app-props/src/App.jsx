9a8318dbd9c7671b26660f3a4f89fe04d66d82d8
Owokade Joseph <80134461+whilly0@users.noreply.github.com>
2025-09-07 23:25:30 +0100

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import UserContext from './UserContext.jsx';
+import ProfilePage from './ProfilePage.jsx';
 
 function App() {
   const userData = {
-    name: "Jane Doe",
-    email: "jane.doe@example.com",
+    name: 'Jane Doe',
+    email: 'jane.doe@example.com',
   };
 
   return (
