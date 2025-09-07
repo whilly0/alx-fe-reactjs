8eb89894c4e37fe0cb75d8925dea292ad7c974ee
Owokade Joseph <80134461+whilly0@users.noreply.github.com>
2025-09-07 20:18:16 +0100

    Create MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: 'lightgray', textAlign: 'left', padding: '20px' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
