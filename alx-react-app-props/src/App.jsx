2938eb8d4edadf74b4a2bf3ed3135a78ae997095
Owokade Joseph <80134461+whilly0@users.noreply.github.com>
2025-09-07 23:46:42 +0100

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import UserContext from './UserContext.jsx';
-import ProfilePage from './ProfilePage.jsx';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
-  const userData = {
-    name: 'Jane Doe',
-    email: 'jane.doe@example.com',
-  };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
